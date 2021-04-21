08e0bd3a36b3bdb75c82c4b5f8b4ce01733ff1a6
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-04-21 23:33:16 +0900

    Create building_co_occurence_matrix.py

---
+++ Natural Language Processing/GloVe : Gloval Vectors for word representation/building_co_occurence_matrix.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def build_co_occurence_mat(tokens, window_size = 4, sym = True):
+    V = np.nunique(tokens)
+    mat = np.zeros((V, V))
+    if sym == True:
+        for i in range(len(tokens)):
+        word_r = tokens[i]
+        if i < window_size:
+            contexts = tokens[:i + window_size + 1]
+        elif i >= window_size and i < len(tokens) - window_size:
+            contexts = tokens[i - window_size:i + window_size + 1]
+        else:
+            contexts = tokens[i - window_size:]
+        counts = dict(zip(*np.unique(contexts, return_counts = True)))
+        for word_c in counts.keys():
+            mat[word_r, word_c] += counts[word_c]
+    return mat
