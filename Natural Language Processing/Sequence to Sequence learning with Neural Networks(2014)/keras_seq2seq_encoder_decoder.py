3861544157346ae1659b33cc0ef8df79712dbc3b
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-06-12 16:33:38 +0900

    Update keras_seq2seq_encoder_decoder.py

--- Natural Language Processing/Sequence to Sequence learning with Neural Networks(2014)/keras_seq2seq_encoder_decoder.py
+++ Natural Language Processing/Sequence to Sequence learning with Neural Networks(2014)/keras_seq2seq_encoder_decoder.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from keras.layers import *
+from tensorflow.keras.layers import *
 
 #0. CONFIG
 N_HIDDEN_STATE = 1000
@@ -12,27 +11,28 @@ EMBED_DIM = 1000
 
 #1. encoder
 enc_input = Input(shape = (MAX_LEN,), name = 'source_input', dtype = 'int32')
-sc_embed = Embedding(S_VOCAB_SIZE+1, EMBED_DIM, name = 'source_embedding')(enc_input)
+sc_embed = Embedding(S_VOCAB_SIZE+1, 1000, name = 'source_embedding')(enc_input)
 x = LSTM(N_HIDDEN_STATE, return_sequences = True, name = 'encoder_lstm_1')(sc_embed)
 x = LSTM(N_HIDDEN_STATE, return_sequences = True, name = 'encoder_lstm_2')(x)
 x = LSTM(N_HIDDEN_STATE, return_sequences = True, name = 'encoder_lstm_3')(x)
-_, state_h, state_c = LSTM(N_HIDDEN_STATE, return_sequences = True, return_state = True, name = 'encoder_lstm_4')(x)
+v, state_h, state_c = LSTM(N_HIDDEN_STATE, return_sequences = False, return_state = True, name = 'encoder_lstm_4')(x)
 encoder = keras.models.Model(enc_input, [state_h, state_c])
+
 
 #2. decoder
 dec_input = Input(shape = (MAX_LEN,), name = 'target_input', dtype = 'int32')
 dec_h_state_input = Input(shape = (N_HIDDEN_STATE,), name = 'decoder_h_state_input')
 dec_c_state_input = Input(shape = (N_HIDDEN_STATE,), name = 'decoder_c_state_input')
-tg_embed = Embedding(T_VOCAB_SIZE+1, EMBED_DIM, name = 'target_embedding')(dec_input)
+tg_embed = Embedding(T_VOCAB_SIZE+1, 1000, name = 'target_embedding')(dec_input)
 y = LSTM(N_HIDDEN_STATE, return_sequences=True, name = 'decoder_lstm_1')(tg_embed, initial_state = [dec_h_state_input, dec_c_state_input])
 y = LSTM(N_HIDDEN_STATE, return_sequences=True, name = 'decoder_lstm_2')(y)
 y = LSTM(N_HIDDEN_STATE, return_sequences=True, name = 'decoder_lstm_3')(y)
 y, dec_h_state, dec_c_state  = LSTM(N_HIDDEN_STATE, return_sequences=True, return_state = True, name = 'decoder_lstm_4')(y)
-y = TimeDistributed(Dense(T_VOCAB_SIZE, activation = 'softmax'))(y)
+y = TimeDistributed(Dense(T_VOCAB_SIZE+1, activation = 'softmax'))(y)
 decoder = keras.models.Model(inputs = [dec_input, dec_h_state_input, dec_c_state_input], outputs = [y, dec_h_state, dec_c_state])
 
-#3. encoder-decoder for training
 
+#3. encoder-decoder for training
 # get layers from encoder and decoder
 sc_embedding = encoder.get_layer('source_embedding')
 tg_embedding = decoder.get_layer('target_embedding')
