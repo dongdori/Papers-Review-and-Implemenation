ba1cfe90e3aeef0dd2aac63853d45a02c49767d2
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-07-09 23:08:05 +0900

    Update keras_transformer_implementation.py

--- Natural Language Processing/(2017)Attention is all you need/keras_transformer_implementation.py
+++ Natural Language Processing/(2017)Attention is all you need/keras_transformer_implementation.py
@@ -4,6 +4,51 @@ from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 
+# utils
+## 1. Functions for positional encoding(sinusodial)
+def get_angles(pos, i, d_model):
+  angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+  return pos * angle_rates
+
+def positional_encoding(position, d_model):
+  angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                          np.arange(d_model)[np.newaxis, :],
+                          d_model)
+
+  # apply sin to even indices in the array; 2i
+  angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+
+  # apply cos to odd indices in the array; 2i+1
+  angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+  pos_encoding = angle_rads[np.newaxis, ...]
+
+  return tf.cast(pos_encoding, dtype=tf.float32)
+
+## 2. Functions for creating masks
+def create_look_back_mask(size):
+  mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+  return mask  # (seq_len, seq_len)
+
+def create_padding_mask(seq):
+  seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+  # add extra dimensions to add the padding
+  # to the attention logits.
+  return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+def create_masks(inp, tar):
+  # Encoder padding mask
+  enc_padding_mask = create_padding_mask(inp)
+  # decoder padding mask applied on encoder output(2nd multihead attention layer)
+  dec_padding_mask = create_padding_mask(inp)
+  # decoder masking on 1st multihead attention layer
+  look_back_mask = create_look_back_mask(tf.shape(tar)[1])
+  dec_target_padding_mask = create_padding_mask(tar)
+  # if token is [PAD] or future token -> masking
+  combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+
+  return enc_padding_mask, combined_mask, dec_padding_mask
+
 
 # Token Embedding + Sinusodial positional embedding Block
 class TokenAndPositionEmbedding(Layer):
@@ -12,16 +57,7 @@ class TokenAndPositionEmbedding(Layer):
         self.d_model = d_model
         self.maxlen = maxlen
         self.token_emb = Embedding(input_dim = vocab_size, output_dim = d_model)
-    def build(self, input_shape):
-        # build sinusoid positional encodings
-        encodings = np.zeros((self.maxlen, self.d_model))
-        for pos in range(self.maxlen):
-            for i in range(self.d_model):
-                if i % 2 == 0:
-                    encodings[pos, i] = np.sin(pos / 10000**(2*i/self.maxlen))
-                else:
-                    encodings[pos, i] = np.cos(pos / 10000**(2*i/self.maxlen))
-        self.pos_encoding = tf.Variable(initial_value = encodings, dtype = np.float32, trainable = False)
+        self.pos_encoding = positional_encoding(self.maxlen, self.d_model)
     def call(self, x):
         token_emb = self.token_emb(x)
         return self.pos_encoding + token_emb
@@ -42,9 +78,9 @@ class EncoderBlock(Layer):
         self.positionalfeedforward_2 = Conv1D(filters = d_model, kernel_size = 1, activation = 'linear')
         self.layernorm_1 = LayerNormalization()
         self.layernorm_2 = LayerNormalization()
-    def call(self, inputs):
+    def call(self, inputs, padding_mask):
         # multihead attention & skip connection
-        out1 = self.multiheadattention(query = inputs, value = inputs, key = inputs)
+        out1 = self.multiheadattention(query = inputs, value = inputs, key = inputs, attention_mask = padding_mask)
         out1 = self.layernorm_1(inputs + out1)
         # positional feedforward 
         out2 = self.positionalfeedforward_1(out1)
@@ -71,12 +107,12 @@ class DecoderBlock(Layer):
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-    def call(self, inputs, enc_output, mask):
+    def call(self, inputs, enc_output, padding_mask, look_back_mask):
         # masked multihead attention layer
-        out1 = self.mha1(query = inputs, value = inputs, key = inputs, attention_mask = mask)
+        out1 = self.mha1(query = inputs, value = inputs, key = inputs, attention_mask = look_back_mask)
         out1 = self.layernorm1(inputs + out1)
         # multihead attention layer
-        out2 = self.mha2(query = enc_output, value = enc_output, key = out1)
+        out2 = self.mha2(query = enc_output, value = enc_output, key = out1, attention_mask = padding_mask)
         out2 = self.layernorm2(out1 + out2)
         # positionwise feedforward layer
         out3 = self.positionalfeedforward_1(out2)
@@ -86,31 +122,30 @@ class DecoderBlock(Layer):
         output = self.layernorm_2(out1 + out2)
 
         return output
-   
 # Encoder with n_layers (TokenPostionalEmbedding + Encoder Blocks)
 class Encoder(Layer):
     def __init__(self, n_layers, maxlen, num_heads, d_v, d_model, vocab_size, fnn):
         super(Encoder, self).__init__()
         self.encoderlayers = [EncoderBlock(maxlen, num_heads, d_v, d_model, fnn) for _ in range(n_layers)]
-        self.embedding = TokenAndPositionEmbedding(max_len, vocab_size, d_model)
-    def call(self, inputs):
+        self.encoding = TokenAndPositionEmbedding(maxlen, vocab_size, d_model)
+    def call(self, inputs, padding_mask):
         # token embedding + positional embedding
-        x = self.embedding(inputs)
+        x = self.encoding(inputs)
         for encoderlayer in self.encoderlayers:
-            x = encoderlayer(x)
-        return x    
+            x = encoderlayer(x, padding_mask)
+        return x        
 # Decoder with n_layers (TokenPositionalEmbedding + Decoder Blocks)
 class Decoder(Layer):
     def __init__(self, n_layers, maxlen, num_heads, d_v, d_model, vocab_size, fnn):
         super(Decoder, self).__init__()
         self.decoderlayers = [DecoderBlock(maxlen, num_heads, d_v, d_model, fnn) for _ in range(n_layers)]
-        self.embedding = TokenAndPositionEmbedding(maxlen, vocab_size, d_model)
-    def call(self, inputs, enc_output, mask):
-        x = self.embedding(inputs)
+        self.encoding = TokenAndPositionEmbedding(maxlen, vocab_size, d_model)
+    def call(self, inputs, enc_output, padding_mask, look_back_mask):
+        x = self.encoding(inputs)
         for decoderlayer in self.decoderlayers:
-            x = decoderlayer(x, enc_output, mask)
+            x = decoderlayer(x, enc_output, padding_mask, look_back_mask)
         return x
-    
+
 # Transformer (Encoder + Decoder + output layer)    
 class TransFormer(Model):
     def __init__(self, n_layers, num_heads, d_v, d_model, tar_vocab_size, src_vocab_size, tar_maxlen, src_maxlen, fnn):
@@ -118,8 +153,8 @@ class TransFormer(Model):
         self.Encoder = Encoder(n_layers, src_maxlen, num_heads, d_v, d_model, src_vocab_size, fnn)
         self.Decoder = Decoder(n_layers, tar_maxlen, num_heads, d_v, d_model, tar_vocab_size, fnn)
         self.output_layer = Dense(tar_vocab_size, activation = 'softmax')
-    def call(self, input_src, input_tar, look_ahead_mask):
-        enc_output = self.Encoder(input_src)
-        dec_output = self.Decoder(input_tar, enc_output, look_ahead_mask)
+    def call(self, input_src, input_tar, look_back_mask, enc_padding_mask, dec_padding_mask):
+        enc_output = self.Encoder(input_src, enc_padding_mask)
+        dec_output = self.Decoder(input_tar, enc_output, dec_padding_mask, look_ahead_mask)
         output = self.output_layer(dec_output)
         return output
