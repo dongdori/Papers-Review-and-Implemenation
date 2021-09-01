e10f4162dfcf2c3bfc8ffc2a7288d1d7626c50a1
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-09-01 15:51:29 +0900

    Create Config.py

---
+++ Generative Model/2014_DCGAN/Config.py
@@ -0,0 +1,13 @@
+batch_size = 64
+image_size = 64
+# dimension of noise vector
+n_z = 100
+# final featuremaps of generator
+n_gf = 128
+# final featurmaps of discriminator
+n_df = 64
+# channel of output image
+nc = 3
+beta_1 = 0.5
+lr = 0.0002
+ngpu = 2
