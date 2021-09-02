f0b802e2c94783efd9958a2b8213f929f082fb6b
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-09-02 21:15:18 +0900

    Update __init__.py

--- Generative Model/2014_DCGAN/__init__.py
+++ Generative Model/2014_DCGAN/__init__.py
@@ -14,19 +14,4 @@ from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_epochs = 30
-batch_size = 64
-image_size = 64
-# dimension of noise vector
-n_z = 100
-# final featuremaps of generator
-n_gf = 128
-# final featurmaps of discriminator
-n_df = 64
-# channel of output image
-nc = 3
-beta_1 = 0.5
-lr = 0.0002
-ngpu = 2
-
 device = torch.device('cuda' if (torch.cuda.is_available() and ngpu > 0) else 'cpu')
