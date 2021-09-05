b70b010796c72fdcad7870597dee97eb4a4eb6cb
Dong Jun <70640345+dongdori@users.noreply.github.com>
2021-09-05 20:00:46 +0900

    Update models.py

--- Generative Model/2017_CycleGAN/models.py
+++ Generative Model/2017_CycleGAN/models.py
@@ -3,9 +3,10 @@ class Generator(nn.Module):
     def __init__(self, n_residual_blocks = 6):
         super(Generator, self).__init__()
         # downsampling
-        layers = [nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 7),
+        layers = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 7),
                  nn.InstanceNorm2d(64),
-                 nn.ReLU(inplace = True),
+                 nn.ReLU(),
                  DownBlock(64, 128),
                  DownBlock(128, 256)]
         # residual layers
@@ -13,10 +14,10 @@ class Generator(nn.Module):
             layers += [ResidualBlock(256)]
         # upsampling
         upblocks_layer = [UpBlock(256, 128),
-                    UpBlock(128, 64),
-                    nn.ReflectionPad2d(3),
-                    nn.Conv2d(64, 3, 7),
-                    nn.Tanh()]
+                          UpBlock(128, 64),
+                          nn.ReflectionPad2d(3),
+                          nn.Conv2d(64, 3, 7),
+                          nn.Tanh()]
         layers += upblocks_layer
         self.model = nn.Sequential(*layers)
     def forward(self, input):
@@ -29,7 +30,7 @@ class DownBlock(nn.Module):
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, stride = 2, padding = 1),
             nn.InstanceNorm2d(out_channels), 
-            nn.ReLU(inplace = True)
+            nn.ReLU()
             )
     def forward(self, input):
         return self.block(input)
@@ -41,21 +42,21 @@ class ResidualBlock(nn.Module):
             nn.ReflectionPad2d(1),
             nn.Conv2d(channels, channels, 3),
             nn.InstanceNorm2d(channels),
-            nn.ReLU(inplace = True),
+            nn.ReLU(),
             nn.ReflectionPad2d(1),
             nn.Conv2d(channels, channels, 3),
             nn.InstanceNorm2d(channels)
         )
     def forward(self, input):
-        return input + self.block(input)
-
+        output =  input + self.block(input)
+        return output
 class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UpBlock, self).__init__()
         self.block = nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, 3, 2, padding = 1, output_padding = 1),
             nn.InstanceNorm2d(out_channels),
-            nn.ReLU(inplace = True),
+            nn.ReLU(),
         )
     def forward(self, input):
         return self.block(input)
@@ -82,13 +83,13 @@ class Discriminator(nn.Module):
     def downsample(self, input_channel, output_channel):
         if input_channel == 3:
             layers = [nn.Conv2d(input_channel, output_channel, kernel_size = 4, stride = 2, padding = 1),
-                      nn.LeakyReLU(0.2, inplace = True)]
+                      nn.LeakyReLU(0.2)]
         elif output_channel == self.channels[-1]:
             layers = [nn.Conv2d(input_channel, output_channel, kernel_size = 4, padding = 1),
                         nn.InstanceNorm2d(output_channel),
-                        nn.LeakyReLU(0.2, inplace = True)]
+                        nn.LeakyReLU(0.2)]
         else:
             layers = [nn.Conv2d(input_channel, output_channel, kernel_size = 4, stride = 2, padding = 1),
                         nn.InstanceNorm2d(output_channel),
-                        nn.LeakyReLU(0.2, inplace = True)]
+                        nn.LeakyReLU(0.2)]
         return layers
